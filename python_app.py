696185b0dc659cb05763db193c2e4baefd018241
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-11-18 08:22:34 +0100

    first python projects

---
+++ python_app.py
@@ -0,0 +1,60 @@
+#define the functions needed: add, sub, mul, div
+#print option to the user
+#ask for values
+#call the functions
+#loop
+
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
+
+def div(a, b):
+    answer = a / b
+    print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer) + "\n")
+
+while True:
+    print("A, Addition")
+    print("B, Subtraction")
+    print("C, Division")
+    print("D, Multiplication")
+
+
+    choice = input("input your choice: ")
+
+    if choice == "a" or choice == "A":
+        print("Addition")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        sub(a, b)
+
+    elif choice == "b" or choice =="B":
+        print("Subtraction")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        sub(a, b)
+
+
+    elif choice == "c" or choice =="C":
+        print("Division")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        div(a, b)
+
+    elif choice == "d" or choice =="D":
+        print("Multiplication")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        mul(a, b)
+
+    elif choice =="e" or choice == "E":
+        print("Program ended")
+        quit(2)
